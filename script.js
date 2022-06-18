bb4166a8a5808ed0ce25d1d147a34c9aeb2e504a
m00nberri <zaberkat@gmail.com>
2022-06-18 13:46:03 +0900

    Add functionality for size slider

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 let canvas = document.getElementById('canvas');
+let slider = document.getElementById('sizeInput');
+let sizeText = document.getElementById('sizeText');
+slider.value = 25;
+sizeText.textContent = `Size: ${slider.value}x${slider.value}`;
+
+buildCanvas(25);
+
+function removePixels() {
+    let pixel = canvas.firstElementChild;
+    while (pixel) {
+        canvas.removeChild(pixel);
+        pixel = canvas.firstElementChild;
+    }
+}
 
 function buildCanvas (size) {
+    removePixels();
     canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`
     for (row=1; row <= size; row++) {
         for (column=1; column <= size; column++) {
             let pixel = document.createElement('div');
-            pixel.className = 'pixel';
+            pixel.className = 'bgPixel';
             canvas.appendChild(pixel);
         }
     }
 }
 
-buildCanvas(10);
+slider.oninput = () => {
+    sizeText.textContent = `Size: ${slider.value}x${slider.value}`;
+}
