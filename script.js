018e5462f5278dea79d9b4b31784ff9924037650
m00nberri <zaberkat@gmail.com>
2022-06-27 12:31:52 +0900

    Add border constrast control to bg color change

--- script.js
+++ script.js
@@ -14,11 +14,32 @@ drawColor.oninput = () => {
 
 }
 
+function changeDraw(color) {
+    drawColor.value = color;
+}
+
 function changeBg(color) {
     let bgPixels = document.getElementsByClassName('bgPixel');
     (Array.from(bgPixels)).forEach(element => {
         element.style.backgroundColor = color;
     });
+}
+
+function setContrast(color) {
+    let pixelBorders = document.getElementsByClassName('pixelBorder');
+    let bg = hex2rgb(color);
+    console.log(bg);
+    let brightness = (0.2126*bg['r'] + 0.7152*bg['g'] + 0.0722*bg['b']);
+    if (brightness <= 50) {
+        (Array.from(pixelBorders)).forEach(element => {
+            element.style.border = 'white solid 0.1px';
+        });
+    }
+    else {
+        (Array.from(pixelBorders)).forEach(element => {
+            element.style.border = 'black solid 0.1px';
+        });
+    }
 }
 
 function removePixels() {
@@ -44,4 +65,17 @@ function buildCanvas (size) {
 
 slider.oninput = () => {
     sizeText.textContent = `Size: ${slider.value}x${slider.value}`;
+}
+
+function hex2rgb(hex) {
+    var validHEXInput = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    if (!validHEXInput) {
+        return false;
+    }
+    var output = {
+        r: parseInt(validHEXInput[1], 16),
+        g: parseInt(validHEXInput[2], 16),
+        b: parseInt(validHEXInput[3], 16),
+    };
+    return output;
 }
